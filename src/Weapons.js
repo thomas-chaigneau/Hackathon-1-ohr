4d761c66e5631c58f3bbd35b9de30054964fe437
eledx <eleonore.dupoix@gmail.com>
2018-10-31 00:00:20 +0100

    ++

--- src/Weapons.js
+++ src/Weapons.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './style/App.css';
 
 class Weapons extends React.Component {
 
@@ -11,31 +12,48 @@ class Weapons extends React.Component {
 		  imgSrc: null
 		}
 
-		// this.showMenu = this.showMenu.bind(this)
-		// this.closeMenu = this.closeMenu.bind(this)
 	}
 
-	// showMenu(event) {
-	// 	event.preventDefault()
-	// 	this.setState({showMenu:true}, () => {
-	// 		document.addEventListener('click',this.closeMenu)
-	// 	})
-	// }
-
-	// closeMenu(){
-	// 	this.setState({showMenu: false}, () => {
-	// 		document.addEventListener('click', this.showMenu)
-	// 	})
-	// } 
 	
 	showMenu = () => {
 		this.setState({showMenu: !this.state.showMenu})
 	}
 
+	loadSable = () => {
+		this.setState({imgSrc: './img/sand.png'})
+	}
 
-	// changeImg = () => {
-	// 	this.setState({})
-	// }
+	loadMarionette = () => {
+		this.setState({imgSrc: './img/marionette.jpg'})
+	}
+
+	loadCouteau = () => {
+		this.setState({imgSrc: './img/couteau-beurre.jpg'})
+	}
+
+	loadLancePierres = () => {
+		this.setState({imgSrc: './img/lance-pierre.jpg'})
+	}
+
+	loadMartinet = () => {
+		this.setState({imgSrc: './img/martinet.jpg'})
+	}
+
+	loadDague = () => {
+		this.setState({imgSrc: './img/dague.jpg'})
+	}
+
+	loadEpee = () => {
+		this.setState({imgSrc: './img/epee.jpg'})
+	}
+
+	loadPiege = () => {
+		this.setState({imgSrc: './img/piege-a-loups.jpg'})
+	}
+
+	loadFusil = () => {
+		this.setState({imgSrc: './img/fusil.jpg'})
+	}
 
 	render() {
 
@@ -46,27 +64,26 @@ class Weapons extends React.Component {
 					Selectionne ton arme
 				</button>
 				{
-				this.state.showMenu !== null
+				this.state.showMenu 
 				? (
-				<div>
-					<div className="menu">
-						<button onClick={this.setState({imgSrc:'./img/sand.png'})}>Sable</button>
-						<button onClick={this.setState({imgSrc:'./img/marionette.jpg'})}>Marionette</button>
-						<button onClick={this.setState({imgSrc:'./img/couteau-beurre.jpg'})}>Couteau à beurre</button>
-						<button onClick={this.setState({imgSrc:'./img/lance-pierre.jpg'})}>Lance-pierres</button>
-						<button onClick={this.setState({imgSrc:'./img/martinet.jpg'})}>Martinet</button>
-						<button onClick={this.setState({imgSrc:'./img/dague.jpg'})}>Dague</button>
-						<button onClick={this.setState({imgSrc:'./img/epee.jpg'})}>Réplique d'épée médiévale</button>
-						<button onClick={this.setState({imgSrc:'./img/piege-a-loups.jpg'})}>Piège à loup</button>
-						<button onClick={this.setState({imgSrc:'./img/fusil.jpg'})}>Fusil d'assaut silencieux</button>
-					</div>
 					<div>
-					{/* <img src={require(this.state.imgSrc)} /> */}
-					{this.state.imgSrc !== null && <img src={require('./img/sand.png')} />}
+						<div className="menu">
+							<button onClick={this.loadSable}>Sable</button>
+							<button onClick={this.loadMarionette}>Marionette</button>
+							<button onClick={this.loadCouteau}>Couteau à beurre</button>
+							<button onClick={this.loadLancePierres}>Lance-pierres</button>
+							<button onClick={this.loadMartinet}>Martinet</button>
+							<button onClick={this.loadDague}>Dague</button>
+							<button onClick={this.loadEpee}>Réplique d'épée médiévale</button>
+							<button onClick={this.loadPiege}>Piège à loup</button>
+							<button onClick={this.loadFusil}>Fusil d'assaut silencieux</button>
+						</div>
+						<div>
+							{this.state.imgSrc !== null && <img src={require(`${this.state.imgSrc}`)} className="weaponsImg"/>}
+						</div>
+						
+						
 					</div>
-					
-					
-				</div>
 				) 
 				: (
 					null
