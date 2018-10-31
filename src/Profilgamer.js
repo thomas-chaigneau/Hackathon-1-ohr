5a4b8580a24352cc04de97ca98be6143feff9c2f
Thomas Chaigneau <thomas.a.chaigneau@gmail.com>
2018-10-31 04:12:14 +0100

    CSS

--- src/Profilgamer.js
+++ src/Profilgamer.js
@@ -5,6 +5,7 @@ class Profilgamer extends Component {
     render() {
         return (
             <div className='profilgamer-container' >
+                <h3>Votre profil de voleur de bonbon</h3>
                 <img src="https://svgsilh.com/svg_v2/152866.svg" alt="avatar du personage" />
                 <p>{this.props.pseudo || 'My Pseudo'}</p>
                 <p>{this.props.category || 'no category' }</p>
